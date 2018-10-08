7d8802fa17d36d799bb105acfac759546697ddf7
Qikai GU <qikai.gu88@gmail.com>
2018-10-08 16:15:23 +0200

    added main

--- src/main/scala/sample/Main.scala
+++ src/main/scala/sample/Main.scala
@@ -1,5 +1,29 @@
 package sample
 
+import org.apache.spark.sql.SparkSession
+import sample.models.CollaborativeFiltering
+
 object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("sample")
+      .master("local[2]")
+      .getOrCreate()
+
+
+    val loader = new Loader(spark)
+    val ratings = loader.loadRatings("input/ratings.csv")
+    val evalRatings = loader.loadEvalRatings("input/evaluation_ratings.csv")
+
+    val cf = new CollaborativeFiltering()
+    cf
+      .init()
+      .fit(ratings)
+      .transform(evalRatings)
+      .submit("output/submission.csv")
+  }
 
 }
